503965ee232fc5a9e7af7183e8ff9197444f1b98
fangxk <joker_fxk@163.com>
2022-04-11 12:38:15 +0800

    project init

---
+++ src/test/java/com/fangxk/xmovie/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fangxk.xmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
